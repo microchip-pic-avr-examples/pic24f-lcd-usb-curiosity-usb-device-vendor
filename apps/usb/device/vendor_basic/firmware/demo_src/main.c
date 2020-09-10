02636b847f6cf3f62d33ab763109c353dac28933
Harish Agari <Harish.Agari@microchip.com>
2020-09-10 14:22:45 +0530

    Removed the unwanted files

--- apps/usb/device/vendor_basic/firmware/demo_src/main.c
+++ apps/usb/device/vendor_basic/firmware/demo_src/main.c
@@ -19,10 +19,8 @@ please contact mla_licensing@microchip.com
 
 /** INCLUDES *******************************************************/
 #include "system.h"
-
 #include "app_device_vendor_basic.h"
 #include "app_led_usb_status.h"
-
 #include "usb.h"
 #include "usb_device.h"
 #include "usb_device_generic.h"
